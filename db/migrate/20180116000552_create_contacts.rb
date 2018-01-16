b1873bc36e067974d54517185d73e1ba0874336c
Alan <alan@Alan-Lee-MacBookPro.local>
2018-01-15 16:35:44 -0800

    added the first_name last_name etc columns to the contact table migration file wiithout running the migration yet

--- db/migrate/20180116000552_create_contacts.rb
+++ db/migrate/20180116000552_create_contacts.rb
@@ -1,7 +1,14 @@
 class CreateContacts < ActiveRecord::Migration[5.1]
   def change
     create_table :contacts do |t|
-
+      t.primary_key "contact_id"
+      t.string      "first_name",   :default => 'unknown',  :null => false
+      t.string      "last_name",    :default => 'unknown',  :null => false
+      t.string      "middle_name"
+      t.string      "other_name"
+      t.string      "name_prefix"
+      t.string      "name_suffix"
+      t.string      "head_shot_link"
       t.timestamps
     end
   end
