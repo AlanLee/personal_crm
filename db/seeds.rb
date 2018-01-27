8a783bfba2a2aa0853902fe2ba6ec7a6afdbbbf0
Alan <alanleeusa@gmail.com>
2018-01-26 17:22:02 -0800

    update seed file using ' contact.notes << Note.create( :body => 'likes football' ) ' etc

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,33 @@ PhoneNumber.delete_all
 Note.delete_all
 Action.delete_all
 
-Contact.create( :first_name => 'Alan' )
-Contact.create( :first_name => 'Henry' )
-Contact.create( :first_name => 'Heidi' )
+Contact.create( :first_name => 'Alan', :last_name => 'See' )
 
-Note.create( :body => 'Part-time videographer', :contact_id => Contact.find_by_first_name("Alan").id )
-Note.create( :body => 'has 2 children', :contact_id => Contact.find_by_first_name("Alan").id )
-Note.create( :body => 'loves studying physics', :contact_id => Contact.find_by_first_name("Alan").id )
+alanlee = Contact.new
+  alanlee.first_name = 'Alan'
+  alanlee.last_name  =  'Lee'
+  alanlee.save
+  alanlee.notes << Note.create( :body => 'is a part-time videographer' )
+  alanlee.notes << Note.create( :body => 'been to Hawaii once' )
+  alanlee.notes << Note.create( :body => 'wants to lose 10 lbs. but can\'t seem to' )
+  alanlee.notes << Note.create( :body => 'has two children; one in Cambridge, the other in San Francisco' )
+  alanlee.notes << Note.create( :body => 'doesn\t watch much TV except for USC football' )
+  alanlee.notes << Note.create( :body => 'has lots of books in the large bookcase' )
+
+henrylee = Contact.new
+  henrylee.first_name = 'Henry'
+  henrylee.last_name = 'Lee'
+  henrylee.save
+  henrylee.notes << Note.create( :body => 'has two degrees from Stanford University' )
+  henrylee.notes << Note.create( :body => 'sold OrderAhead in 2017 to Square Inc.' )
+  henrylee.notes << Note.create( :body => 'played a lot of soccer but it tapered off while he was in college' )
+  henrylee.actions << Action.create( :should_do => 'meet for coffee and discuss music' )
+  henrylee.actions << Action.create( :should_do => 'check if Henry needs funding later this 2018 year' )
+  henrylee.actions << Action.create( :should_do => 'does Henry play golf? should invite him sometime' )
+  # henrylee.phone_numbers << PhoneNumber.create( :call_number => 9497519287 )
+
+
+Contact.create( :first_name => 'Heidi' )
 
 Note.create( :body => 'loves music', :contact_id => Contact.find_by_first_name("Henry").id )
 Note.create( :body => 'lives in San Francisco', :contact_id => Contact.find_by_first_name("Henry").id )
